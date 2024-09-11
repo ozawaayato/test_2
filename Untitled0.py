5273936146b38fa0bebfbd59619e62b424b0ffbb
ozawaayato <ozawa-ayato@metawater.co.jp>
2024-09-11 11:37:59 +0900

    Colab を使用して作成されました

---
+++ Untitled0.py
@@ -0,0 +1,48 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyO7PPVuYHU6jJxv2XdqX+eL",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/ozawaayato/test_2/blob/main/Untitled0.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "markdown",
+      "source": [
+        "#テスト"
+      ],
+      "metadata": {
+        "id": "S70F8q0d8YKG"
+      }
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "ChNSh-KQ6G2g"
+      },
+      "outputs": [],
+      "source": []
+    }
+  ]
+}
